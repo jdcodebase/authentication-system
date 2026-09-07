7a5be3660e92bc38190184e4bfd598d0699df6d2
Jatin Dhamija (Jd) <jd.codebase@gmail.com>
2026-09-07 14:10:03 +0530

    fix: remove unused email configuration variables

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -49,11 +49,8 @@ const env = {
   EMAIL_CHANGE_TOKEN_SECRET: process.env.EMAIL_CHANGE_TOKEN_SECRET,
   EMAIL_CHANGE_TOKEN_EXPIRATION: process.env.EMAIL_CHANGE_TOKEN_EXPIRATION,
 
-  EMAIL_HOST: process.env.EMAIL_HOST,
-  EMAIL_PORT: process.env.EMAIL_PORT,
   EMAIL_USER: process.env.EMAIL_USER,
   EMAIL_PASS: process.env.EMAIL_PASS,
-  EMAIL_FROM: process.env.EMAIL_FROM,
 
   NODE_ENV: process.env.NODE_ENV,
 };
