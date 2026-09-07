8a5e5309af137cab3dc44a9d033254ccb890a24e
Jatin Dhamija (Jd) <jd.codebase@gmail.com>
2026-09-07 13:33:10 +0530

    fix: revert email transporter to use Gmail service configuration

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -3,9 +3,7 @@ import env from "./env.js";
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-  host: env.EMAIL_HOST,
-  port: Number(env.EMAIL_PORT),
-  secure: Number(env.EMAIL_PORT) === 465,
+  service: "gmail",
   auth: {
     user: env.EMAIL_USER,
     pass: env.EMAIL_PASS,
