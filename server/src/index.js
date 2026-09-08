5468d03f3933e9496211f6e6fbf72a4e03850b38
Jatin Dhamija (Jd) <jd.codebase@gmail.com>
2026-09-08 12:54:24 +0530

    fix: ensure email transporter verification is awaited in server startup

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,7 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    verifyEmailTransporter();
+    await verifyEmailTransporter();
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
